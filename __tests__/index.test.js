37ac98eb8a4166bd1f60947631d6263a6a0182cc
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-09-20 16:20:09 +0300

    add reading from file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,24 @@
 import reverse from '../src/index.js';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { test, expect } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
+  expect(reverse('string')).toEqual('gnirts');
+  expect('').toEqual('');
+});
+
+test('reverse with log', () => {
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+  const text = readFile('input.txt');
+  const result = readFile('output.txt');
+
+  expect(reverse(text)).toEqual(result);
 });
